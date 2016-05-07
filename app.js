3ecb0f5ccabf299be982d1c989cb1d2e260040d0
System User <you@example.com>
2016-05-07 11:35:31 -0500

    Test malos para probar algo

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 var express = require('express');
+var appempic = require('./lib/appempic');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.send('Hello World '+ appempic.getName() + " " + appempic.getLastName());
 });
 
 app.listen(app.get('port'), function() {
